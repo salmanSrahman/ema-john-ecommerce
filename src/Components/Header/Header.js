9e0bb103a652ae1ed857282754c636ae3a7d3f2c
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-21 12:05:20 +0600

    header part end

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,23 +2,56 @@ import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import "./Header.css";
+import logo from "../../images/logo.png";
 
 const Header = () => {
   return (
-    <div>
+    <div className="header-part">
       <Navbar bg="light" expand="lg">
-        <Container fluid>
-          <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Container>
+          <Navbar.Brand href="#">
+            <img src={logo} className="header-logo" alt="" />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav
-              className="me-auto my-2 my-lg-0"
+              className="ms-auto my-2 my-lg-0"
               style={{ maxHeight: "100px" }}
               navbarScroll
             >
-              <NavLink to="/shop">Shop</NavLink>
-              <NavLink to="/order">Order Review</NavLink>
-              <NavLink to="/inventory">Manage Inventory</NavLink>
+              <NavLink
+                style={({ isActive }) => {
+                  return {
+                    color: isActive ? "goldenrod" : "black",
+                    textDecoration: isActive ? "underline" : "NONE",
+                  };
+                }}
+                to="/shop"
+              >
+                Shop
+              </NavLink>
+              <NavLink
+                style={({ isActive }) => {
+                  return {
+                    color: isActive ? "goldenrod" : "black",
+                    textDecoration: isActive ? "underline" : "NONE",
+                  };
+                }}
+                to="/order"
+              >
+                Order Review
+              </NavLink>
+              <NavLink
+                style={({ isActive }) => {
+                  return {
+                    color: isActive ? "goldenrod" : "black",
+                    textDecoration: isActive ? "underline" : "NONE",
+                  };
+                }}
+                to="/inventory"
+              >
+                Manage Inventory
+              </NavLink>
             </Nav>
           </Navbar.Collapse>
         </Container>
