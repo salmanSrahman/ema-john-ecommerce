af31471deedb9130fffe5edb0468b6e81669ee27
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-21 13:12:34 +0600

    fetch data & all products shown in ui

--- src/Components/Products/Product/Product.js
+++ src/Components/Products/Product/Product.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "./Product.css";
 
-const Product = () => {
+const Product = ({ product }) => {
+  console.log(product);
+  const { img, name, price, seller, star, starCount, stock } = product;
   return (
-    <div>
-      <h1>Here is product.</h1>
+    <div className="product-container">
+      <div className="product-img">
+        <img src={img} alt="" className="img-fluid" />
+      </div>
+      <div className="product-details d-flex align-items-center">
+        <div>
+          <h5 className="text-primary fw-bold fs-5">{name}</h5>
+          <h5>By: {seller}</h5>
+          <h5>${price}</h5>
+          <h5>
+            only {stock} left in stock -{" "}
+            <span className="text-danger">order soon</span>
+          </h5>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import "./Shop.css";
+import Product from "../Products/Product/Product";
 
 const Shop = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("./products.JSON")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
   return (
     <div>
-      <h1>Here Is Shop.</h1>
+      <Container>
+        <Row>
+          <Col md={9}>
+            <div className="products-container">
+              {products.map((product) => (
+                <Product product={product}></Product>
+              ))}
+            </div>
+          </Col>
+          <Col md={3}>
+            <div className="cart-container"></div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
